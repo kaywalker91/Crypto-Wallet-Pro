4ae77811a179dc924f7a0315f9f41504ddeaff79
KayWalker <kaywalker@KayWalkerui-MacBookPro.local>
2025-12-09 17:29:18 +0900

    chore(android): set qr scanner namespace

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -15,8 +17,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (name == "qr_code_scanner") {
+            extensions.findByType(LibraryExtension::class.java)?.let { ext ->
+                if (ext.namespace == null) {
+                    ext.namespace = "net.touchcapture.qr.flutterqr"
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
